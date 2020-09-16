5481784590270d1bc6103f83caf0b216715bbe8b
Raphael <raphaelmalinski@gmail.com>
2020-09-16 19:38:25 -0300

    print_array function created

--- CPD/Quick Sort/q1.cpp
+++ CPD/Quick Sort/q1.cpp
@@ -7,8 +7,18 @@
 
 using namespace std;
 
+void print_array(int C[], int tam)
+{
+	printf("[");
+	for (int i = 0; i < tam - 1; i++)
+	{
+		printf("%d, ", C[i]);
+	}
+	printf("%d]", C[tam - 1]);
+}
 
-int main(int argc, const char * argv[]){
-	
-    return 0;
+int main(int argc, const char *argv[])
+{
+
+	return 0;
 }
