7fd4bc7cec882cc2c46f3b08c56b60613e85b34b
Raphael <raphaelmalinski@gmail.com>
2020-09-16 20:10:53 -0300

    Fixed the partition function

--- CPD/Quick Sort/q1.cpp
+++ CPD/Quick Sort/q1.cpp
@@ -9,63 +9,73 @@ using namespace std;
 
 clock_t start_clock, end_clock;
 
-void print_array(int C[], int tam) {
+void print_array(int C[], int tam)
+{
 	printf("[");
-	for (int i=0; i<tam-1; i++) {
+	for (int i = 0; i < tam - 1; i++)
+	{
 		printf("%d, ", C[i]);
 	}
-	printf("%d]", C[tam-1]);
+	printf("%d]", C[tam - 1]);
 }
 
 float left, right, max;
 
-
-int partition(int C[], int left, int right) {
-	int chave, i, j;
-	chave = C[right]; i = left; j = right-1;
-	int loopcount=0;
-	for (;;) {
+int partition(int C[], int left, int right)
+{
+	int chave, i;
+	chave = C[right];
+	i = left;
+	int loopcount = 0;
+	for (int j = left;j < right; j++)
+	{
 		//cout << "\nloop = " << loopcount++ << "i - " << i << " j = " << j << endl;
-		while(C[i] <= chave) i++ ;
-		while(C[j] > chave) j--;
-		if (i >= j) break;
-		std::swap (C[i], C[j]);
+		if(C[j] < chave){
+			std::swap(C[i], C[j]);
+			i++;
+		}
 	}
-	std::swap (C[i], C[right]);
+	std::swap(C[i], C[right]);
 	return i;
 }
 
-void quick_sort_rec (int C[], int left, int right) { 
+void quick_sort_rec(int C[], int left, int right)
+{
 	int pivot; // índice do pivô
-	if (right > left) {
-		pivot = partition (C, left, right);
-		quick_sort_rec (C, left, pivot-1); 
-		quick_sort_rec (C, pivot+1, right);
-	} 
+	if (right > left)
+	{
+		pivot = partition(C, left, right);
+		quick_sort_rec(C, left, pivot - 1);
+		quick_sort_rec(C, pivot + 1, right);
+	}
 }
 
-void quick_sort (int C[], int tam, int print_ok) {
-	if (print_ok) {
+void quick_sort(int C[], int tam, int print_ok)
+{
+	if (print_ok)
+	{
 		printf("\nArray before:");
 		print_array(C, tam);
 	}
-	start_clock = clock ();
-	quick_sort_rec(C, 0, tam-1);
+	start_clock = clock();
+	quick_sort_rec(C, 0, tam - 1);
 	end_clock = clock();
-	if (print_ok) {
+	if (print_ok)
+	{
 		printf("\nArray after:");
 		print_array(C, tam);
 	}
-	printf("\nQuick Sort time = %f seconds", 
-		(end_clock - start_clock)/(double)CLOCKS_PER_SEC );	
+	printf("\nQuick Sort time = %f seconds",
+		   (end_clock - start_clock) / (double)CLOCKS_PER_SEC);
 }
 
 #define SIZE 100
 
-int main(int argc, const char * argv[]){
-	int arr1[10]={12,2,5,4,8,7,6,9,1,15};
-	int arr2[10]={1,2,3,4,5,6,7,8,9,10};
-	int arr3[10]={10,9,8,7,6,5,4,3,2,1};
+int main(int argc, const char *argv[])
+{
+	int arr1[10] = {12, 2, 5, 4, 8, 7, 6, 9, 1, 15};
+	int arr2[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int arr3[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 	int arr4[SIZE];
 	int arr5[SIZE];
 	int arr6[SIZE];
@@ -76,29 +86,34 @@ int main(int argc, const char * argv[]){
 	ifstream arquivo_cadeados;
 
 	arquivo_chaves.open("chaves.txt");
-	int i=0;
-	while (arquivo_chaves >> chaves[i++]);
-	printf ("\nRead %d chaves", i-1);
+	int i = 0;
+	while (arquivo_chaves >> chaves[i++])
+		;
+	printf("\nRead %d chaves", i - 1);
 	cout << chaves[0];
 
-
 	arquivo_cadeados.open("cadeados.txt");
-	int j=0;
-	while (arquivo_cadeados >> cadeados[j++]);
-	printf ("\nRead %d cadeados", j-1);
+	int j = 0;
+	while (arquivo_cadeados >> cadeados[j++])
+		;
+	printf("\nRead %d cadeados", j - 1);
 
 	quick_sort(arr1, 10, 1);
 	quick_sort(arr2, 10, 1);
 	quick_sort(arr3, 10, 1);
 
-	printf("\nWorst case");
-	for (int i=0; i<SIZE; i++) arr4[i]=SIZE-i;
-	quick_sort(arr4, SIZE, 0);
+	// printf("\nWorst case");
+	// for (int i = 0; i < SIZE; i++)
+	// 	arr4[i] = SIZE - i;
+	// quick_sort(arr4, SIZE, 0);
 
-	for (int j=0; j<10; j++) {
-		for (int i=0; i<SIZE; i++) arr4[i]=rand()%SIZE+1;	
-		quick_sort(arr4, SIZE, 1);
-		cout << endl << endl;
-	}
-    return 0;
+	// for (int j = 0; j < 10; j++)
+	// {
+	// 	for (int i = 0; i < SIZE; i++)
+	// 		arr4[i] = rand() % SIZE + 1;
+	// 	quick_sort(arr4, SIZE, 1);
+	// 	cout << endl
+	// 		 << endl;
+	// }
+	return 0;
 }
