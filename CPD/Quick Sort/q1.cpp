3904cfad5851e09899af1358ae06bfd78071185e
Raphael <raphaelmalinski@gmail.com>
2020-09-16 19:37:04 -0300

    Created base file

---
+++ CPD/Quick Sort/q1.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main(int argc, const char * argv[]){
+	
+    return 0;
+}
