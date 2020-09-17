9920fb07366fdad0e3ea20fd0067cc58e229eefd
Raphael <raphaelmalinski@gmail.com>
2020-09-17 11:25:06 -0300

    Refactored quick_sort functions

--- CPD/Quick Sort/q1.cpp
+++ CPD/Quick Sort/q1.cpp
@@ -22,32 +22,13 @@ void print_array(int C[], int tam)
 
 float left, right, max;
 
-int partition(int C[], int left, int right)
-{
-	int chave, i;
-	chave = C[right];
-	i = left;
-	int loopcount = 0;
-	for (int j = left; j < right; j++)
-	{
-		//cout << "\nloop = " << loopcount++ << "i - " << i << " j = " << j << endl;
-		if (C[j] < chave)
-		{
-			std::swap(C[i], C[j]);
-			i++;
-		}
-	}
-	std::swap(C[i], C[right]);
-	return i;
-}
-
 int partition_lomuto(int C[], int left, int right)
 {
 	int chave, i;
 	chave = C[left];
-	i = left+1;
+	i = left + 1;
 	int loopcount = 0;
-	for (int j = left+1; j <= right; j++)
+	for (int j = left + 1; j <= right; j++)
 	{
 		//cout << "\nloop = " << loopcount++ << "i - " << i << " j = " << j << endl;
 		if (C[j] < chave)
@@ -56,69 +37,96 @@ int partition_lomuto(int C[], int left, int right)
 			i++;
 		}
 	}
-	std::swap(C[left], C[i-1]);
-	return i-1;
+	std::swap(C[left], C[i - 1]);
+	return i - 1;
 }
 
 int partition_hoare(int C[], int left, int right)
 {
 	int chave, i, j;
 	chave = C[left];
-	i = left-1;
+	i = left - 1;
 	j = right;
 	int loopcount = 0;
-	while(true){
-		while (C[j] >  chave){
+	while (true)
+	{
+		while (C[j] > chave)
+		{
 			j--;
-		} 
-		do{
+		}
+		do
+		{
 			i++;
 		} while (C[i] < chave);
 		if (i >= j)
 		{
 			return j;
 		}
-		std::swap(C[i], C[j]);		
+		std::swap(C[i], C[j]);
 	}
 }
 
 int partition_hoare_median(int C[], int left, int right)
 {
 	int chave, i, j;
-	chave = C[(left+right)/2];
-	i = left-1;
-	j = right +1;
+	chave = C[(left + right) / 2];
+	i = left - 1;
+	j = right + 1;
 	int loopcount = 0;
-	while(true){
-		do{
+	while (true)
+	{
+		do
+		{
 			j--;
-		} while (C[j] >  chave);
-		do{
+		} while (C[j] > chave);
+		do
+		{
 			i++;
 		} while (C[i] < chave);
 		if (i >= j)
 		{
 			return j;
 		}
-		std::swap(C[i], C[j]);		
+		std::swap(C[i], C[j]);
 	}
 }
 
-void quick_sort_rec(int C[], int left, int right)
+int partition_lomuto_random(int C[], int left, int right)
+{
+	int pivot, r;
+	r = rand();
+	pivot = left + r % (right - left + 1);
+	std::swap(C[right], C[pivot]);
+	return partition_lomuto(C, left, right);
+}
+
+void quick_sort_rec_lomuto(int C[], int left, int right)
 {
 	int pivot; // índice do pivô
 	if (right > left)
 	{
-		pivot = partition(C, left, right);
+		//Para testar o random, basta comentar o partition_lomuto e descomentar o partition_lomuto_random
 		//pivot = partition_lomuto(C, left, right);
-		//pivot = partition_hoare(C, left, right);
-		//pivot = partition_hoare_median(C, left, right);
-		quick_sort_rec(C, left, pivot - 1);
-		quick_sort_rec(C, pivot + 1, right);
+		pivot = partition_lomuto_random(C, left, right);
+		quick_sort_rec_lomuto(C, left, pivot - 1);
+		quick_sort_rec_lomuto(C, pivot + 1, right);
 	}
 }
 
-void quick_sort(int C[], int tam, int print_ok)
+void quick_sort_rec_hoare(int C[], int left, int right)
+{
+	int pivot; // índice do pivô
+	if (right > left)
+	{
+		//Para testar o median, basta comentar o partition_hoare e descomentar o partition_hoare_median
+		//pivot = partition_hoare(C, left, right);
+		pivot = partition_hoare_median(C, left, right);
+		quick_sort_rec_lomuto(C, left, pivot);
+		quick_sort_rec_lomuto(C, pivot + 1, right);
+	}
+}
+
+void quick_sort_lomuto(int C[], int tam, int print_ok)
 {
 	if (print_ok)
 	{
@@ -126,7 +134,26 @@ void quick_sort(int C[], int tam, int print_ok)
 		print_array(C, tam);
 	}
 	start_clock = clock();
-	quick_sort_rec(C, 0, tam - 1);
+	quick_sort_rec_lomuto(C, 0, tam - 1);
+	end_clock = clock();
+	if (print_ok)
+	{
+		printf("\nArray after:");
+		print_array(C, tam);
+	}
+	printf("\nQuick Sort time = %f seconds",
+		   (end_clock - start_clock) / (double)CLOCKS_PER_SEC);
+}
+
+void quick_sort_hoare(int C[], int tam, int print_ok)
+{
+	if (print_ok)
+	{
+		printf("\nArray before:");
+		print_array(C, tam);
+	}
+	start_clock = clock();
+	quick_sort_rec_hoare(C, 0, tam - 1);
 	end_clock = clock();
 	if (print_ok)
 	{
@@ -166,9 +193,13 @@ int main(int argc, const char *argv[])
 		;
 	printf("\nRead %d cadeados", j - 1);
 
-	quick_sort(arr1, 10, 1);
-	quick_sort(arr2, 10, 1);
-	quick_sort(arr3, 10, 1);
+	quick_sort_lomuto(arr1, 10, 1);
+	quick_sort_lomuto(arr2, 10, 1);
+	quick_sort_lomuto(arr3, 10, 1);
+
+	quick_sort_hoare(arr1, 10, 1);
+	quick_sort_hoare(arr2, 10, 1);
+	quick_sort_hoare(arr3, 10, 1);
 
 	// printf("\nWorst case");
 	// for (int i = 0; i < SIZE; i++)
