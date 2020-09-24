df2051602e51e0e566dc9d49dd2054e677718bab
Raphael <raphaelmalinski@gmail.com>
2020-09-24 10:44:31 -0300

    Create write_output_file function using radix sort

--- CPD/RadixSort/main.cpp
+++ CPD/RadixSort/main.cpp
@@ -8,61 +8,48 @@
 
 using namespace std;
 
-void alloc_array(char **arr)
+size_t get_bigger_string_length(string arr[], int size)
 {
-    arr = new char *[1000];
-    for (int i = 0; i < 1000; i++)
+    size_t bigger = arr[0].size();
+    for (int i = 1; i < size; i++)
     {
-        arr[i] = new char[30];
+        if (arr[i].size() > bigger)
+            bigger = arr[i].size();
     }
+    return bigger;
 }
 
-void print_array(char arr[][30])
+void countSort(string a[], int size, size_t k)
 {
-    int i = 0;
-    while (arr[i][0] != '\n')
-    {
-        cout << arr[i] << " ";
-        i++;
-    }
-}
-
-
-size_t getMax(string arr[], int n){
-    size_t max = arr[0].size();
-    for (int i = 1; i < n; i++){
-        if (arr[i].size()>max)
-            max = arr[i].size();
-    }
-    return max;
-}
-
-void countSort(string a[], int size, size_t k){
-    string *b = NULL; int *c = NULL;
+    string *b = NULL;
+    int *c = NULL;
     b = new string[size];
     c = new int[257];
 
-
-
-    for (int i = 0; i <257; i++){
+    //initializing arr
+    for (int i = 0; i < 257; i++)
+    {
         c[i] = 0;
-        //cout << c[i] << "\n";
     }
-    for (int j = 0; j <size; j++){   
-        c[k < a[j].size() ? (int)(unsigned char)a[j][k] + 1 : 0]++;            //a[j] is a string
-        //cout << c[a[j]] << endl;
+    for (int j = 0; j < size; j++)
+    {
+        c[k < a[j].size() ? (int)(unsigned char)a[j][k] + 1 : 0]++; //a[j] is a string
+                                                                    //cout << c[j] << endl;
     }
 
-    for (int f = 1; f <257; f++){
+    for (int f = 1; f < 257; f++)
+    {
         c[f] += c[f - 1];
     }
 
-    for (int r = size - 1; r >= 0; r--){
+    for (int r = size - 1; r >= 0; r--)
+    {
         b[c[k < a[r].size() ? (int)(unsigned char)a[r][k] + 1 : 0] - 1] = a[r];
         c[k < a[r].size() ? (int)(unsigned char)a[r][k] + 1 : 0]--;
     }
 
-    for (int l = 0; l < size; l++){
+    for (int l = 0; l < size; l++)
+    {
         a[l] = b[l];
     }
 
@@ -71,56 +58,53 @@ void countSort(string a[], int size, size_t k){
     delete[] c;
 }
 
-
-void radixSort(string b[], int r){
-    size_t max = getMax(b, r);
-    for (size_t digit = max; digit > 0; digit--){ // size_t is unsigned, so avoid using digit >= 0, which is always true
-        countSort(b, r, digit - 1);
+void radixSort(string b[], int size)
+{
+    size_t bigger = get_bigger_string_length(b, size);
+    for (size_t digit = bigger; digit > 0; digit--)
+    {
+        countSort(b, size, digit - 1);
     }
-
 }
 
-void read_input_file(char input[][30], char file_name[])
+int read_input_file(string input[], char file_name[])
 {
-    int i = 0;
+    int num_words, i = 0;
     ifstream input_file;
 
     input_file.open(file_name);
-    while (input_file >> input[i++])
-        ;
-    input[i][0] = '\n';
+    while (input_file >> input[i++]){
+        num_words++;
+    }
+
+    return num_words;
 }
 
-void write_output_file(char input[][30], char file_name[])
+void write_output_file(string input[], char file_name[], int size)
 {
     int i = 0;
     ofstream output_file;
 
     output_file.open(file_name, ios::out);
+    
+    radixSort(input, size);
 
-    //do here the sort
-    radixSort(input, 10000)
-
-    while (input[i][0] != '\n')
+    for (int l = 0; l < size; l++)
     {
-        output_file << input[i];
+        output_file << input[l];
         output_file << ' ';
-        i++;
     }
 }
 
 
-
-
 int main(/*int argc, char const *argv[]*/)
 {
-    char input[100000][30];
-    char input_file_name[50] = "inputs/input.txt";
+    string *input = new string[1000000];
+    char input_file_name[50] = "inputs/war_and_peace_clean.txt";
     char output_file_name[50] = "outputs/output.txt";
 
-    read_input_file(input, input_file_name);
-    write_output_file(input, output_file_name);
-    //print_array(input);
+    int size = read_input_file(input, input_file_name);
+    write_output_file(input, output_file_name, size);
 
     return 0;
 }
