07fd95ba7be0061b0189b9f7968c52e0a7167e25
Raphael <raphaelmalinski@gmail.com>
2020-11-09 23:29:56 -0300

    Fix the logic to read line in csv file

--- CPD/Trabalho final/src/ratings/ratings.cpp
+++ CPD/Trabalho final/src/ratings/ratings.cpp
@@ -19,47 +19,37 @@ void read_ratings()
 	fstream fin;
 	vector<Rating> ratings;
     Rating rating;
-	string line, temp;
+	string line;
     string delimiter = ",";
     size_t pos = 0;
 	
 	fin.open("../Dados_clean/minirating.csv", ios::in);
-	fin >> line;
-	while (fin >> line)
+    getline(fin, line);
+
+	while (getline(fin, line))
 	{
-		fin >> temp;
-		line = line + " " + temp;
-        
         pos = line.find(delimiter);
-        temp = line.substr(0, pos);
-        rating.userId = stoi(temp);
+        rating.userId = stoi(line.substr(0, pos));
         line.erase(0, pos + delimiter.length());
 
         pos = line.find(delimiter);
-        temp = line.substr(0, pos);
-        rating.movieId = stoi(temp);
+        rating.movieId = stoi(line.substr(0, pos));
         line.erase(0, pos + delimiter.length());
 
         pos = line.find(delimiter);
-        temp = line.substr(0, pos);
-        rating.rating = stof(temp);
-        line.erase(0, pos + delimiter.length());
-
-        pos = line.find(delimiter);
-        temp = line.substr(0, pos);
-        rating.timesTamp = temp;
+        rating.rating = stof(line.substr(0, pos));
         line.erase(0, pos + delimiter.length());
 
         ratings.push_back(rating);
 	}
 
 	//Usado pra imprimir
-	for (vector<Rating>::iterator it = ratings.begin(); it != ratings.end(); it++)
-	{
-		cout << 
-        "User Id: " << it->userId << 
-        ", Movie Id: " << it->movieId << 
-        ", Rating: " << it->rating << 
-        ", TimesTemp: " << it->timesTamp << "\n";
-	}
+	// for (vector<Rating>::iterator it = ratings.begin(); it != ratings.end(); it++)
+	// {
+	// 	cout << 
+    //     "User Id: " << it->userId << 
+    //     ", Movie Id: " << it->movieId << 
+    //     ", Rating: " << it->rating << endl;
+    //     //", TimesTemp: " << it->timesTamp << "\n";
+	// }
 }
